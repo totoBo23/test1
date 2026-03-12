785d3de5e5976e814c219b65c8223a47ae951fca
totoBo23 <peioelustondo@gmail.com>
2026-03-12 18:35:09 +0100

    Delete MKT LIT directory

--- MKT LIT/assets/js/config.example.js
+++
@@ -1,3 +0,0 @@
-window.MRG_CONFIG = {
-  googleMapsApiKey: "PASTE_YOUR_GOOGLE_MAPS_API_KEY_HERE"
-};
--- MKT LIT/assets/js/config.js
+++
@@ -1,3 +0,0 @@
-window.MRG_CONFIG = {
-  googleMapsApiKey: "AIzaSyDq11rQki-0mHsjnOGqOS7d-i8IVLd-W-I"
-};
